49d20d7f7a6a4863eb284dc512a2d659e4316f09
Alexey Sas <asasryl@gmail.com>
2017-09-20 18:16:25 +0300

    some stuff

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Dropout
 import keras
 
 
@@ -13,6 +14,9 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    X = [series[i : i + window_size] for i, _ in enumerate(series[ : len(series) - window_size])]
+    y = [item for item in series[ window_size : len(series)]]
+    
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -22,8 +26,14 @@ def window_transform_series(series, window_size):
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
+# Adding dropout decreases train accuracy but increased test accuracy (clearly desired effect to avoid overfitting :)
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dropout(0.2))
+    model.add(Dense(1))
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
